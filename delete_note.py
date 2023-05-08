5e835853d409fb71679e1439448b82c71b288fb2
KlimmedSoul <122864116+KlimmedSoul@users.noreply.github.com>
2023-05-08 13:17:35 +0300

    Delete delete_note.py

--- delete_note.py
+++
@@ -1,6 +0,0 @@
-from execute_read_query import execute_read_query
-
-def delete_note(connection, title): 
-    note_to_delete = f"""DELETE FROM users WHERE title = "{title}";"""
-    execute_read_query(connection, note_to_delete)
-    print("Заметка успешно удалена")
